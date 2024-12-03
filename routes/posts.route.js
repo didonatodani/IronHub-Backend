eca1d0301e33bb417f0b21751b04d0b77dafd945
Nigel Ferreres <nferrefe@gmail.com>
2024-12-03 15:57:39 +0100

    get by search and id

--- routes/posts.route.js
+++ routes/posts.route.js
@@ -2,16 +2,25 @@ const express = require("express");
 const router = express.Router();
 const Post = require("../models/Post.model");
 
+/*Get all post */
 router.get("/", async (req, res, next) => {
   try {
-    const response = await Post.find()
-    res.json(response)
+    const response = await Post.find();
+    if (response) {
+      res.json(response);
+    } else {
+      res
+        .status(404)
+        .json({ message: "There are no posts on forum, create a new one" });
+    }
   } catch (error) {
-    console.log("Error creating a post: ", error);
+    res
+      .status(500)
+      .json({ message: "An error occurred while displaying all the post" });
   }
 });
 
-
+/*Create a post */
 router.post("/", async (req, res, next) => {
   const { name, course, title, description, link, picture, likes } = req.body;
 
@@ -23,15 +32,64 @@ router.post("/", async (req, res, next) => {
     link,
     picture,
     likes,
-};
+  };
 
-try {
+  try {
     const response = await Post.create(newPost);
-    res.json(response)
+    if (response) {
+      res.json(response);
+    } else {
+      res
+        .status(404)
+        .json({ message: "There are no posts on forum, create a new one" });
+    }
   } catch (error) {
-    console.log("Error creating a post: ", error);
+    res
+      .status(500)
+      .json({ message: "An error occurred while creating a the post" });
   }
 });
+
+/*Get a post by search params*/
+
+router.get("/search", async (req, res) => {
+  const { title } = req.query;
+
+  try {
+    const response = await Post.findOne({
+      title: { $regex: title, $options: "i" },
+    });
+
+    if (response) {
+      res.json(response);
+    } else {
+      res.status(404).json({ message: "No post found with the given title" });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "An error occurred while searching for the post" });
+  }
+});
+
+/*Get a post by ID */
+router.get("/:postId", async (req, res, next) => {
+  const { postId } = req.params;
+
+  try {
+    const response = await Post.findById(postId);
+    if (response) {
+      res.json(response);
+    } else {
+      res.status(404).json({ message: "No post found with this ID" });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "An error occurred while searching for the post" });
+  }
+});
+
 
 
 
