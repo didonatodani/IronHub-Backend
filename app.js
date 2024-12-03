3df247d65479c1cf01b3fe77b1708c2b55607d74
Peedee01 <piethein@schouten.nl>
2024-12-03 17:11:25 +0100

    merge conflict

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ const indexRoutes = require("./routes/index.routes");
 app.use("/api", indexRoutes);
 
 const authRoutes = require("./routes/auth.routes");
+const User = require("./models/User.model");
 app.use("/auth", authRoutes);
 
 
