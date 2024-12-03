3f2aa14b034613acac20b9e5d113cafdbe59a2ed
Peedee01 <piethein@schouten.nl>
2024-12-03 14:33:40 +0100

    user workes

--- models/Post.model.js
+++ models/Post.model.js
@@ -1,13 +1,14 @@
 const { Schema, model } = require("mongoose");
 
+
 const postSchema = new Schema({
   name: {
-    type: mongoose.Schema.Types.userId,
+    type: Schema.Types.ObjectId,
     ref: "User",
     required: [true, "Username is required"],
   },
   course: {
-    type: mongoose.Schema.Types.CourseId,
+    type: Schema.Types.ObjectId,
     ref: "Course",
     required: [true, "Course is required"],
   },
