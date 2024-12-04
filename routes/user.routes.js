829d8768f5a258344caaa1d465676b1497e41f49
Nigel Ferreres <nferrefe@gmail.com>
2024-12-04 11:48:56 +0100

    array posts

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -12,6 +12,7 @@ router.get("/:userId", async (req, res) => {
     const response = await User.findById(userId);
     if(response) {
       res.json(response)
+      console.log(response.posts);
     } else {
       res.status(404).json({ message: "No user found with this id"});
     }
@@ -30,6 +31,8 @@ router.put("/:userId", async (req, res) => {
     const response = await User.findByIdAndUpdate(userId, req.body, { new: true });
     if(response) {
       res.json(response)
+      
+      
     } else {
       res.status(404).json({ message: "No user found with this id"});
     }
@@ -48,6 +51,7 @@ router.get("/:userId/posts", async (req, res) => {
   try {
     const response = await User.findById(userId).populate("posts", "posts");
     if(response) {
+      console.log(response.posts)
       res.json(response)
     } else {
       res.status(404).json({ message: "No user found with this id"});
@@ -59,27 +63,5 @@ router.get("/:userId/posts", async (req, res) => {
   }
 });
 
-
-
-
-
-/*Get posts from user */
-// router.get("/:userId/:postId", async (req, res, next) => {
-//   const { userId, postId } = req.params;
-//   try {
-//     const response = await User.findById(userId);
-//     if (response) {
-//       res.json(response);
-//     } else {
-//       res
-//         .status(404)
-//         .json({ message: "There are no posts on forum, create a new one" });
-//     }
-//   } catch (error) {
-//     res
-//       .status(500)
-//       .json({ message: "An error occurred while displaying all the post" });
-//   }
-// });
 
 module.exports = router
